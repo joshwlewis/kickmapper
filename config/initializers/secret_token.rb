6fd7cd322402ee247d0d7f5079a19a22341cb3a9
Josh Lewis <josh.w.lewis@gmail.com>
2012-11-02 20:59:45 -0500

    Create rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kickmapper::Application.config.secret_token = 'd2a2148dd48d08c5f58146ad5e32521088de5efc7b1f92785f0caf406c861bf1305e9b18300070ac14266f0977cddf930e0469baf85193245ebd7926a55df854'
